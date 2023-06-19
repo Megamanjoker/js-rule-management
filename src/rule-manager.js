71af2f57423e46827f7a5735a7722006e3f9bb0a
Tyler <megamanjoker@live.com>
2023-06-19 09:07:01 -0500

    add prettier and change rule-manager.js to follow more inline with the standard array.

--- src/rule-manager.js
+++ src/rule-manager.js
@@ -1,25 +1,24 @@
-const Rule = require('./rule')
+const Rule = require("./rule");
 
 module.exports = class RuleManager extends Array {
-    push(...items) {
-        super.push(...items.flat().map((item)=>{
-            const wrapperRule= new Rule();
-            wrapperRule.input = item;
-            return item.script ? item : wrapperRule;
-        }))
-    }
+  push(...items) {
+    return super.push(
+      ...items.flat().map((item) => {
+        const wrapperRule = new Rule();
+        wrapperRule.input = item;
+        return item.script ? item : wrapperRule;
+      })
+    );
+  }
 
-    runRules(){
-        this.results = this.map((rule)=>{
-            try{
-                rule.script(rule.input)
-                return "passed"
-            }
-            catch (e)
-            {
-                return e
-            }
-        })
-    }
-
-}
+  runRules() {
+    this.results = this.map((rule) => {
+      try {
+        rule.script(rule.input);
+        return "passed";
+      } catch (e) {
+        return e;
+      }
+    });
+  }
+};
